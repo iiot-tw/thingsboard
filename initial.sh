9fde56c1afa4b0d47f4bfb5929b356683901a6db
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-09-24 10:03:03 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,4 +1,4 @@
-ACCESS="PUT_YOUR_GW_ACCESS_TOKEN_HERE"
+ACCESS=$(hexdump -s96 -n16 -e '16/1 "%02X"' /sys/bus/i2c/devices/0-0050/eeprom)
 sudo sed -i 's/PUT_YOUR_GW_ACCESS_TOKEN_HERE/${ACCESS}/' /etc/thingsboard-gateway/config/tb_gateway.yaml
 
 #download config file
