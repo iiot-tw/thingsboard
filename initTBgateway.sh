eee53c76cf23d9507c90b880ca67bb16eaed03b2
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-10-18 21:55:53 +0800

    Update initTBgateway.sh

--- initTBgateway.sh
+++ initTBgateway.sh
@@ -27,3 +27,6 @@ connectors:
 EOF
 
 sudo cp /tmp/tb_gateway.yaml /etc/thingsboard-gateway/config/tb_gateway.yaml
+sudo mkdir -p /usr/lib/python3/dist-packages/thingsboard_gateway/extensions/di
+sudo cp ./custom_di_connector.py /usr/lib/python3/dist-packages/thingsboard_gateway/extensions/di
+sudo cp ./custom_di.json /etc/thingsboard-gateway/config/
