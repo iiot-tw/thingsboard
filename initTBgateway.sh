9be1f1d62919f941325b08c056cefe19f9631dfa
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-10-18 21:21:07 +0800

    Update initTBgateway.sh

--- initTBgateway.sh
+++ initTBgateway.sh
@@ -1,3 +1,6 @@
+wget https://github.com/thingsboard/thingsboard-gateway/releases/download/2.5.1/python3-thingsboard-gateway.deb
+sudo apt install ./python3-thingsboard-gateway.deb -y
+
 HOST=cloud.iiot.tw
 TOKEN=$(sudo ~/igtA token)
 sudo sed -i "s,host: demo.thingsboard.io,host: $HOST,g" /etc/thingsboard-gateway/config/tb_gateway.yaml
