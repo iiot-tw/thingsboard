d73a3f6b0a66afa938f8789170233bedbdaaa97e
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-10-20 12:11:53 +0800

    Update custom_di_connector.py

--- custom_di_connector.py
+++ custom_di_connector.py
@@ -38,31 +38,55 @@ class CustomDIConnector(Thread, Connector):
     def run(self):    # Main loop of thread
         log.debug("CustomDIConnector run")
         while True:
-            for device in self.__config["devices"]:
+            for device in self.__devices:
+                current_time = time.time()
+                to_send = {}
                 log.debug(type(device))
                 log.debug(device)
-                for channel in device["timeseries"]:
-                    log.debug(type(channel))
-                    log.debug(channel)
-                    address = channel["address"]
-                    log.debug(type(address))
-                    log.debug(address)
-                    devFile = "/sys/class/gpio/gpio" + str(address) + "/value"
-                    log.debug(type(devFile))
-                    log.debug(devFile)
-                    f = open(devFile, "r")
-                    ret= f.read()
-                    f.close()
-                    log.debug(type(ret))
-                    log.debug(ret)
-                    log.debug(type(device.get('deviceName')))
-                    log.debug(device.get('deviceName'))
-                    to_send = {'deviceName': device.get('deviceName'), 'deviceType': 'default', 'telemetry': [{channel["tag"]: int(ret)}], 'attributes': []    }
-                    log.debug(type(to_send))
-                    log.debug(to_send)
-                    self.__gateway.send_to_storage(self.get_name(), to_send)
-            time.sleep(1)
+                try:
+                    if self.__devices[device]["config"].get("timeseries") is not None:
+                        if self.__devices[device]["next_timeseries_check"] < current_time:
+                            #  Reading data from device
+                            tmp_list = []
+                            for index in range(len(self.__devices[device]["config"]["timeseries"])):
+                                channel = self.__devices[device]["config"]["timeseries"][index] # channel is a dict
+                                address = channel["address"]
+                                devFile = "/sys/class/gpio/gpio" + str(address) + "/value"
+                                f = open(devFile, "r")
+                                ret= f.read()
+                                f.close()
+                                tmp_list.append({channel["tag"]: int(ret)})
 
+                            self.__devices[device]["next_timeseries_check"] = current_time + self.__devices[device]["config"]["timeseriesPollPeriod"]/1000
+
+                            if self.__devices[device]["config"].get("sendDataOnlyOnChange"):
+                                self.statistics['MessagesReceived'] += 1
+                                to_send = {'deviceName': device, 'deviceType': 'default'}
+                                if to_send.get("telemetry") is None:
+                                    to_send["telemetry"] = []
+                                if to_send.get("attributes") is None:
+                                    to_send["attributes"] = []
+                                for telemetry_dict in tmp_list:
+                                    for key, value in telemetry_dict.items():
+                                        if self.__devices[device]["last_telemetry"].get(key) is None or \
+                                           self.__devices[device]["last_telemetry"][key] != value:
+                                            self.__devices[device]["last_telemetry"][key] = value
+                                            to_send["telemetry"].append({key: value})
+                                if not to_send.get("attributes") and not to_send.get("telemetry"):
+                                    log.debug("Data has not been changed.")
+                            elif self.__devices[device]["config"].get("sendDataOnlyOnChange") is None or not self.__devices[device]["config"].get("sendDataOnlyOnChange"):
+                                self.statistics['MessagesReceived'] += 1
+                                to_send = {'deviceName': device, 'deviceType': 'default'}
+                                to_send["telemetry"] = tmp_list
+                                to_send["attributes"] = []
+
+                    if to_send.get("attributes") or to_send.get("telemetry"):
+                        log.debug(to_send)
+                        self.__gateway.send_to_storage(self.get_name(), to_send)
+                        self.statistics['MessagesSent'] += 1
+
+                except Exception as e:
+                    log.exception(e)
 
     def close(self):    # Close connect function, usually used if exception handled in gateway main loop or in connector main loop
         self.stopped = True
@@ -79,5 +103,9 @@ class CustomDIConnector(Thread, Connector):
             for device in self.__config["devices"]:
                 if device.get('deviceName') not in self.__gateway.get_devices():
                     self.__gateway.add_device(device.get('deviceName'), {"connector": self})
+                self.__devices[device["deviceName"]] = { "config": device,
+                                                         "next_timeseries_check": 0, # for timeseriesPollPeriod
+                                                         "last_telemetry": {} # for sendDataOnlyOnChange
+                                                         }
         except Exception as e:
             log.exception(e)
