3e1031d7998bb730136feba3ea132a442bffe212
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-10-22 19:00:00 +0800

    Create localip.sh

---
+++ localip.sh
@@ -0,0 +1,8 @@
+ipSafe=$(ip a | grep 'inet ' | \
+       cut -f6 -d' ' | tr '\n' ' ' | \
+       sed 's|\/|@|g' | sed 's| |\&|g')
+
+echo "{\"localIp\":\"$ipSafe\", \"localIpLastUpdate\":\"$(date)\"}" > /dev/shm/IP
+if [ -f "/dev/shm/IP" ]; then
+  curl -X POST http://cloud.iiot.tw/api/v1/$(sudo /neousys/igtInfo token)/attributes --header "Content-Type:application/json" -d @/dev/shm/IP
+fi
