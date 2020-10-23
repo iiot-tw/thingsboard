eabd73d95abbd61570afe0dfb19faf29a297e998
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-10-23 19:52:35 +0800

    Update igt-startup.sh

--- igt-startup.sh
+++ igt-startup.sh
@@ -2,7 +2,7 @@ log="igt_startup:"
 echo "${log} begin"
 #your code...
 /neousys/usb.sh
-/neousys/localip.sh
+/neousys/tbClient-igtInfo.sh
 chmod 666 /dev/ttyS2
 crontab */10 * * * * /neousys/tbClient-igtInfo.sh
 echo "${log} end"
