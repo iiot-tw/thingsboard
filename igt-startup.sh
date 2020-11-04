0c5e28e72725228f72cc57fb9e67901194bab55c
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-11-04 16:00:01 +0800

    change mode of gpio files to 666
    enable access from NodeRED in docker

--- igt-startup.sh
+++ igt-startup.sh
@@ -5,4 +5,14 @@ echo "${log} begin"
 /neousys/tbClient-igtInfo.sh
 chmod 666 /dev/ttyS2
 crontab */10 * * * * /neousys/tbClient-igtInfo.sh
+igt22 do_en 1
+chmod 666 /sys/class/gpio/gpio111/value
+chmod 666 /sys/class/gpio/gpio110/value
+chmod 666 /sys/class/gpio/gpio112/value
+chmod 666 /sys/class/gpio/gpio113/value
+chmod 666 /sys/class/gpio/gpio117/value
+chmod 666 /sys/class/gpio/gpio104/value
+chmod 666 /sys/class/gpio/gpio7/value
+chmod 666 /sys/class/gpio/gpio103/value
+#docker start igtnodered
 echo "${log} end"
