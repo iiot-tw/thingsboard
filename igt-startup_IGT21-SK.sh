0a7edf44950a22911fa9b0bc9ca7d039d7463176
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-11-25 17:20:16 +0800

    Create igt-startup_IGT21-SK.sh

---
+++ igt-startup_IGT21-SK.sh
@@ -0,0 +1,14 @@
+log="igt_startup:"
+echo "${log} begin"
+#your code...
+/neousys/tbClient-igtInfo.sh
+igt21 ttys2 485
+chmod 666 /dev/ttyS2
+crontab */10 * * * * /neousys/tbClient-igtInfo.sh
+igt21 do_en 1
+chmod 666 /sys/class/gpio/gpio44/value
+chmod 666 /sys/class/gpio/gpio45/value
+chmod 666 /sys/class/gpio/gpio46/value
+chmod 666 /sys/class/gpio/gpio47/value
+#docker start igtnodered
+echo "${log} end"
