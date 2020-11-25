b80489de223e68c2fe2b5e1e4ec1ca1470c9a71a
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2020-11-25 16:36:40 +0800

    Delete initial.sh

--- initial.sh
+++
@@ -1,5 +0,0 @@
-ACCESS=$(hexdump -s96 -n16 -e '16/1 "%02X"' /sys/bus/i2c/devices/0-0050/eeprom)
-sudo sed -i 's/PUT_YOUR_GW_ACCESS_TOKEN_HERE/${ACCESS}/' /etc/thingsboard-gateway/config/tb_gateway.yaml
-
-#download config file
-#sudo wget https://raw.githubusercontent.com/iiot-tw/thingsboard/master/CG_ESTHW50A2D.json -O /etc/thingsboard-gateway/config/CG_ESTHW50A2D.json
