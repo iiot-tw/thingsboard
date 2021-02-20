cab23e5467082a5970ace7d46cf0d5f720d1f183
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2021-02-20 10:44:23 +0800

    Update tbClient-igtInfo.sh

--- tbClient-igtInfo.sh
+++ tbClient-igtInfo.sh
@@ -9,5 +9,5 @@ echo "{\"localIp\":\"$ipSafe\", \
        \"serialNum\":\"$(sudo /neousys/igtInfo serial)\" \
       }" > /dev/shm/igtInfo.json
 if [ -f "/dev/shm/igtInfo.json" ]; then
-  curl -X POST http://${PLACE_HOST}/api/v1/${PLACE_TOKEN}/attributes --header "Content-Type:application/json" -d @/dev/shm/igtInfo.json
+  curl -X POST http://##PLACE_HOST##/api/v1/##PLACE_TOKEN##/attributes --header "Content-Type:application/json" -d @/dev/shm/igtInfo.json
 fi
